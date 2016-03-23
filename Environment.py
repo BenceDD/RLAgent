3e02a4a5bcf305680e791e63e5963624c1c01445
Bence Mikulas <bence.mikulas@sap.com>
2016-03-23 18:21:30 +0100

    Environment and Memory implementation added

--- Environment.py
+++ Environment.py
@@ -1,21 +1,58 @@
+import random
+
+
 class Environment:
-    pass
-
-
-class EnvironmentImplementation(Environment):
-
-    def __init__(self):
-        # sample data...
-        self._internal_state = 4
-        self._description = ['partially observable', 'deterministic', 'episodic', 'static', 'discrete']
-        self._actions = ['left', 'right', 'up', 'down']
-
-    def get_environment_description(self):
-        return self._description
 
     def observe(self):
-        # a list describe the internal state, a reward for the last experiment, and the actions for next
-        return {'state': self._internal_state/2,  'reward': 1, 'actions': self._actions}
+        pass
 
     def apply_action(self, action):
-        self._internal_state += action
+        pass
+
+
+class WoodCutterEnvironment(Environment):
+
+    def __init__(self):
+        self._money = 100
+        self._tree_age = -1
+        self._storm_happened = False  # this is just for printing
+        self._actions = ['plant tree', 'cut out tree', 'wait']
+
+    def observe(self):
+        if self._storm_happened is True:
+            print('Your trees destroyed!! :(')
+
+        if self._tree_age == -1 or self._storm_happened is True:
+            print('You have no trees.')
+        else:
+            print('Your trees ' + str(self._tree_age) + ' years old.')
+
+        print('Your money is: ' + str(self._money))
+
+        if self._tree_age == -1 or self._storm_happened is True:
+            actions = ['plant tree', 'wait']
+        else:
+            actions = ['cut out tree', 'wait']
+
+        return self._tree_age, actions, self._money
+
+    def apply_action(self, action):
+
+        if self._storm_happened is True:
+            self._storm_happened = False
+
+        if action == 'plant tree' and self._tree_age == -1 and self._money >= 50:
+            self._money -= 50
+            self._tree_age = 0
+            print('Trees planted!')
+        if action == 'cut out tree' and self._tree_age != -1:
+            self._money += (self._tree_age * 20)
+            print('Trees has cut down. You earned: ' + str(self._tree_age * 20))
+            self._tree_age = -1
+        if action == 'wait':
+            self._tree_age += 1
+            print('Waiting for one year...')
+
+        if random.randint(0, 7) is 2:
+            self._tree_age = -1
+            self._storm_happened = True
