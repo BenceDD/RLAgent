697a05fe6292d59c8f3feec540a032506c446020
Bence Mikulas <bence.mikulas@sap.com>
2016-04-29 12:18:15 +0200

    Comments added

--- Architecture.py
+++ Architecture.py
@@ -24,17 +24,37 @@ class DiscreteActionHandler:
         self._action_table = defaultdict(lambda: lambda: print("Unimplemented action!"))
 
     def sample(self, action_name):
+        """
+        Get the action for the execution by index/id.
+        :param action_name: Action index/id
+        :return: The action with the given name
+        """
         return self._action_table[action_name]
 
     def set_action_handler(self, action_name, action_handler):
+        """
+        Set action for an action ID.
+        :param action_name: Index on the action
+        :param action_handler: A function which has to be executed for the given action ID/index
+        :return: None
+        """
         self._action_table[action_name] = action_handler
 
     def get_interpretation_interval(self):
+        """
+        Returns all the possible action ID's.
+        :return: List of possible keys
+        """
         return self._action_table.keys()
 
 
 class ContinuousActionHandler:
     def __init__(self, name, resolution):
+        """
+        Makes a new manipulator with continuous interpretation range
+        :param name: name of the manipulator, should be unique for the architecture instance
+        :param resolution: sampling resolution of the function
+        """
         self.name = name
         self.resolution = resolution
         self._action_table = defaultdict(lambda: lambda: print("Unimplemented action!"))
@@ -46,19 +66,26 @@ class ContinuousActionHandler:
                 return self._action_table[points]
 
     def set_action_handler(self, interval, action_handler):
-        # interval is a tuple
+        """
+        Set action for an interval.
+        :param interval: tuple of integers
+        :param action_handler: a function
+        :return:
+        """
         (begin, end) = interval
         if end < begin:
             raise Exception("Starting point of interval must be lower than ending point!")
 
         for i in self._action_table:
-            if i.begin <= interval.end or interval.begin <= i.end:
+            (i_begin, i_end) = i
+            if i_begin <= interval.end or interval.begin <= i_end:
                 raise Exception("Intervals shouldn't overlap each other!")
 
         self._action_table[interval] = action_handler
 
     def get_interpretation_interval(self):
-        return {range(i[0], i[1], self.resolution) for i in self._action_table}
+        for begin, end in self._action_table:
+            for
 
 
 handler = ContinuousActionHandler("szintszabalyozo", 0.5)
